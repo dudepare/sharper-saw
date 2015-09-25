efd8e99c832b06ad804834360fd8798c3fc949f4
Andrew Artajos <andrew.artajos.dev@gmail.com>
2015-09-25 12:41:23 +1000

    trailing zeros exercise

---
+++ python/CONTENTS.md
@@ -0,0 +1,8 @@
+# Contents
+
+Here I do my best to describe the things that live in this directory.
+
+## trailing zeroes
+
+*Requirements*
+* how would you count the number of trailing zeroes in !100 (100 factorial)?
---
+++ python/trailing000.py
@@ -0,0 +1,35 @@
+import unittest
+
+
+def count_endzeros(bignumber):
+    count = 0
+    num = bignumber
+    while num % 10 == 0:
+        count += 1
+        num = num // 10
+    return count
+
+
+class TrailingZeroTest(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_nozero(self):
+        self.assertEqual(count_endzeros(1324324),0)
+
+    def test_onezero(self):
+        self.assertEqual(count_endzeros(232140), 1)
+
+    def test_fivezero(self):
+        self.assertEqual(count_endzeros(12000200000), 5)
+
+    def test_lotsozero(self):
+        self.assertEqual(
+            count_endzeros(100000000000000000000000000), 26)
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
